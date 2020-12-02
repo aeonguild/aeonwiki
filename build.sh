8c54a8dcb09562f8ecc0d9952ce2365078e4792d
ivoryguru <ivoryguru@pm.me>
2020-12-02 23:50:45 +0000

    update

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-cd chart_maker/
+cd chart-maker/
 python3 update.py
 cd ..
 rm -r docs/network-info/charts/*/
-cp -r chart_maker/charts/* docs/network-info/charts/
+cp -r chart-maker/charts/* docs/network-info/charts/
 
-./node_scraper/moneriote.sh
-mv node_scraper/open-nodes.md docs/network-info/
+./node-scraper/moneriote.sh
+mv node-scraper/open-nodes.md docs/network-info/
 
 python3 -m mkdocs build
 rm -r /var/www/aeon.wiki/html/*
---
+++ node-scraper/moneriote.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#Gingeropolous Open node checker
+
+# This is the directory where files are written to.
+# If you run as a cronjob, you have to use the full path
+DIR=/root/aeonwiki/node-scraper
+
+# This is the path for your monerod binary.
+aeond=/root/aeon/build/release/bin/aeond
+
+# This is the ip of your local daemon. If you're not running an open node, 127.0.0.1 is fine.
+daemon=0.0.0.0
+
+#Where you're going to dump the file that will be published
+html_dir=/root/aeonwiki/node-scraper
+
+# Bound rpc port
+bport=32405
+
+#Port to sniff for
+port=11181
+
+echo $aeond
+echo $daemon
+
+###
+
+cd $DIR
+rm open-nodes.md
+
+echo "# Open Nodes" >> open-nodes.md
+echo "" >> open-nodes.md
+echo "##" >> open-nodes.md
+echo "" >> open-nodes.md
+echo "##############"
+echo "Check network white nodes for domains to add"
+
+#launch a local daemon instance which will connect to another running daemon and request the current peer list
+#note that if the other daemon is running with the "restricted-rpc option", it will ignore "print_pl"
+white=$($aeond --rpc-bind-ip $daemon --rpc-bind-port $bport print_pl | grep white | awk '{print $3}' | cut -f 1 -d ":")
+
+
+white_a=($white)
+echo ${white_a[@]}
+echo "################"
+echo ${#white_a[@]}
+
+echo "#############"
+echo "Starting loop"
+
+ctr=0
+
+echo "Number of nodes: "${#white_a[@]} >> moneriote.log
+
+
+#Comment out to check within the loop, and uncomment the one below
+l_hit="$(curl -X POST http://$daemon:$bport/getheight -H 'Content-Type: application/json' | grep height | cut -f 2 -d : | cut -f 1 -d ,)"
+
+for i in "${white_a[@]}"
+do
+   : 
+	echo "Checking ip: "$i
+	#Uncomment the below to check within the loop
+	#l_hit="$(curl -X POST http://$daemon:$bport/getheight -H 'Content-Type: application/json' | grep height | cut -f 2 -d : | cut -f 1 -d ,)"
+  #attempt to connect to each node, allowing 0.5 seconds for connection
+	r_hit="$(curl -m 0.5 -X POST http://$i:$port/getheight -H 'Content-Type: application/json' | grep height | cut -f 2 -d : | cut -f 1 -d ,)"
+	echo "Local Height: "$l_hit
+	echo "Remote Height: "$r_hit
+        mini=$(( $l_hit-10 ))
+        echo "minimum is " $mini
+        maxi=$(( $l_hit+10 ))
+        echo "max is " $maxi
+        if [[ "$r_hit" ==  "$l_hit" ]] || [[ "$r_hit" > "$mini" && "$r_hit" < "$maxi" ]] && [[ -n $r_hit ]] && [[ -n $l_hit ]]
+        then
+        echo "################################# Daemon $i is good" 
+        if [[ $i == 142.93.155.14 ]]
+        then
+        echo "* $i aeon.wiki"  >> open-nodes.md
+        elif [[ $i != 0.0.0.0 ]] 
+        then
+        HOST=$(host $i | awk '{print substr($NF, 1, length($NF)-1)}')
+        echo "* $i ${HOST}"  >> open-nodes.md
+        fi
+	elif [[ $r_hit ]] || [[ $l_hit ]]; then
+	echo "Either the local or remote is dead"
+	else
+	echo "$i is closed"
+	fi
+done
